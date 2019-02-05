20814de227f8d99e76d435aeaaf47cb00a8dbb05
davidfloyd91 <davidfloyd91@gmail.com>
2019-02-05 10:05:36 -0500

    Done.

--- spec/controllers/categories_controller_spec.rb
+++ spec/controllers/categories_controller_spec.rb
@@ -1,6 +1,24 @@
 require 'rails_helper'
 
 RSpec.describe CategoriesController, type: :controller do
+
+  ### monkeypatch
+  if RUBY_VERSION>='2.6.0'
+    if Rails.version < '5'
+      class ActionController::TestResponse < ActionDispatch::TestResponse
+        def recycle!
+          # hack to avoid MonitorMixin double-initialize error:
+          @mon_mutex_owner_object_id = nil
+          @mon_mutex = nil
+          initialize
+        end
+      end
+    else
+      puts "Monkeypatch for ActionController::TestResponse no longer needed"
+    end
+  end
+  ###
+
   describe "GET #show" do
     it "returns HTTP success" do
       get :show, id: @category
